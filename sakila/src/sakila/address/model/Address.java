6faa6ee19ad002417bd715894179869178bc3fb2
wuim <wuim@DESKTOP-SR66FKI>
2019-10-05 16:03:22 +0900

    address,city,country에 클래스 필드 생성

--- sakila/src/sakila/address/model/Address.java
+++ sakila/src/sakila/address/model/Address.java
@@ -1,5 +1,67 @@
 package sakila.address.model;
 
 public class Address {
-
+	private int addressId;
+	private String address;
+	private String address2;
+	private String distirct;
+	private City city;
+	private String postalCode;
+	private String phone;
+	private String lastUpdate;
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getDistirct() {
+		return distirct;
+	}
+	public void setDistirct(String distirct) {
+		this.distirct = distirct;
+	}
+	public int getCityId() {
+		return cityId;
+	}
+	public void setCityId(int cityId) {
+		this.cityId = cityId;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(String lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	@Override
+	public String toString() {
+		return "Address [addressId=" + addressId + ", address=" + address + ", address2=" + address2 + ", distirct="
+				+ distirct + ", cityId=" + cityId + ", postalCode=" + postalCode + ", phone=" + phone + ", lastUpdate="
+				+ lastUpdate + "]";
+	}
+	
 }
--- sakila/src/sakila/address/model/City.java
+++ sakila/src/sakila/address/model/City.java
@@ -1,5 +1,38 @@
 package sakila.address.model;
 
 public class City {
-
+	private int cityId;
+	private String city;
+	private Country country;
+	private String lastUpdate;
+	public int getCityId() {
+		return cityId;
+	}
+	public void setCityId(int cityId) {
+		this.cityId = cityId;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getCountryId() {
+		return countryId;
+	}
+	public void setCountryId(int countryId) {
+		this.countryId = countryId;
+	}
+	public String getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(String lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	@Override
+	public String toString() {
+		return "City [cityId=" + cityId + ", city=" + city + ", countryId=" + countryId + ", lastUpdate=" + lastUpdate
+				+ "]";
+	}
+	
 }
