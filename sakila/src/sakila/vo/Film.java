12b62573989f5704222969877c16d294b1e12a8c
GD3 <GD3@DESKTOP-H9MC3L3>
2019-11-20 20:38:32 +0900

    film,filmactor,actor,language 테이블 vo 생성(set,get,toString)

---
+++ sakila/src/sakila/vo/Film.java
@@ -0,0 +1,45 @@
+package sakila.vo;
+
+public class Film {
+	private int filmId;
+	private String title;
+	private String description;
+	private String releaseYear;
+	private Language language;
+	public int getFilmId() {
+		return filmId;
+	}
+	public void setFilmId(int filmId) {
+		this.filmId = filmId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getReleaseYear() {
+		return releaseYear;
+	}
+	public void setReleaseYear(String releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+	public Language getLanguage() {
+		return language;
+	}
+	public void setLanguage(Language language) {
+		this.language = language;
+	}
+	@Override
+	public String toString() {
+		return "Film [filmId=" + filmId + ", title=" + title + ", description=" + description + ", releaseYear="
+				+ releaseYear + ", language=" + language + "]";
+	}
+	
+}
