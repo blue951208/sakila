d1f3db794aa07dca33e02737e4804467929be485
wuim <wuim@DESKTOP-SR66FKI>
2019-11-26 23:02:38 +0900

    businessList,customerList,inventoryList.html 생성 select로 선택된 value값을 ajax로 넘긴다

---
+++ sakila/src/sakila/controller/GetFilmbyStore.java
@@ -0,0 +1,22 @@
+package sakila.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetFilmbyStore
+ */
+@WebServlet("/getFilmbyStore")
+public class GetFilmbyStore extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+response.setContentType("Application/json");
+		
+		int storeId = Integer.parseInt(request.getParameter("storeId"));
+		System.out.println("controlller store film:"+storeId);
+	}
+
+}
