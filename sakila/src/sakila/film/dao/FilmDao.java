6cfb860e5ec2c6449bd05924fec871cf3accccfc
GD3 <GD3@DESKTOP-H9MC3L3>
2019-11-26 20:02:58 +0900

    쿼리문 문법 오류 수정완료, 페이지에 출력 문제 발생

--- sakila/src/sakila/film/dao/FilmDao.java
+++ sakila/src/sakila/film/dao/FilmDao.java
@@ -5,27 +5,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 import sakila.db.DBHelper;
+import sakila.vo.Actor;
 import sakila.vo.Film;
 
 public class FilmDao {
 	//선택한 배우번호에 따른 영화리스트 출력
 	public List<Film> selectFilmbyActor(String name){
 		System.out.println("dao film:"+name);
-		String word = "'%"+name+"%'";
-		System.out.println(">>:"+word);
+	
 		List<Film> list = new ArrayList<Film>();
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
-		String sql = "select f.title,f.description,f.release_year"
-		+" from film f inner join film_actor fa inner join actor a"
-		+" on a.actor_id=fa.actor_id and f.film_id=fa.film_id"
-		+" where a.first_name like ? or a.last_name like ?";
+		String sql = "select "
+				+"f.title,f.description,f.release_year "
+				+"from film f inner join film_actor fa inner join "
+				+"actor a  on a.actor_id=fa.actor_id and f.film_id=fa.film_id "
+				+"where concat(first_name,'',last_name) like concat('%',?,'%')";
+		
 		try {
 			conn = DBHelper.getConnection();
 			stmt = conn.prepareStatement(sql);
-			stmt.setString(1,word);
-			stmt.setString(2,word);
+			stmt.setString(1,name);
+			System.out.println("sql:>"+sql);
 			rs = stmt.executeQuery();
 			if(rs.next()) {
 				Film film = new Film();
